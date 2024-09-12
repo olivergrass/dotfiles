3ef58811dc00e7acc501cb68e1163dbf384f9b2e
olivergrass <olivergrass2001@gmail.com>
2024-09-12 16:55:41 +0200

    add install script

---
+++ install_apps.sh
@@ -0,0 +1,25 @@
+# System update
+sudo apt update
+
+# Install Rust
+curl https://sh.rustup.rs -sSf | sh
+
+# Apps
+sudo apt install -y exa
+sudo apt install -y zoxide
+sudo apt install -y chezmoi
+sudo apt install -y thefuck
+
+cargo install git-delta
+
+# Glow .md renderer
+# sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
+
+# Install starship prompt
+curl -sS https://starship.rs/install.sh | sh
+
+# Install node version manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
